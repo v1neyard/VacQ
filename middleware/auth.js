d9dffc211246c830e55383b82a4947e6189eeeb3
vineyard <vineyard.achi@gmail.com>
2024-02-27 15:05:38 +0700

    final

--- middleware/auth.js
+++ middleware/auth.js
@@ -28,7 +28,7 @@ exports.protect = async (req, res, next) => {
 
 exports.authorize = (...roles) => {
     return (req, res, next) => {
-        if (!roles.includes(req.ussr.role)){
+        if (!roles.includes(req.user.role)){
             return res.status(403).json({
                 success: false,
                 msg: `User role ${req.user.role} is not authorized to access this route`
